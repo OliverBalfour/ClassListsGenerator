516f644d21ed7bd6e4dd928954e3313471b17318
Oliver Balfour <oliver.leo.balfour@gmail.com>
2020-06-29 21:56:38 +1000

    Updates

---
+++ precache-manifest.70ff077ebacf4810dc0061afeee50010.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c68b11906df7f3c6e8a7af177d3cb1e",
+    "url": "/ClassListsGenerator/index.html"
+  },
+  {
+    "revision": "9679b5120f5a9749b346",
+    "url": "/ClassListsGenerator/static/js/2.665b019d.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/ClassListsGenerator/static/js/2.665b019d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf65925827bbebaf0c7",
+    "url": "/ClassListsGenerator/static/js/main.e71e459c.chunk.js"
+  },
+  {
+    "revision": "7b12fa222d60bb6b6266",
+    "url": "/ClassListsGenerator/static/js/runtime-main.7685c764.js"
+  }
+]);
